a4b7e35ed2440ffb753dc9672ec4d01a27131b21
Maxxo0 <135011110+Maxxo0@users.noreply.github.com>
2024-05-03 13:33:42 +0200

    bullet1

---
+++ Transmission Takedown/Assets/TT_Root/Scripts/Managers/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+
+                Debug.Log("GameManager is null!");
+            }
+            return instance;
+        }
+    }
+
+    public enum Weapons { gun, bomb, arm, car }
+
+    public Weapons weapon;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+        
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
