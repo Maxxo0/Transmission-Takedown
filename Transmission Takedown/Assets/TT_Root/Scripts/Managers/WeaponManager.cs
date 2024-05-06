b70f3d7a214497a2ca40d5354b53b9f5f8614b4f
Maxxo0 <135011110+Maxxo0@users.noreply.github.com>
2024-05-06 12:39:20 +0200

    Gun 1

--- Transmission Takedown/Assets/TT_Root/Scripts/Managers/WeaponManager.cs
+++ Transmission Takedown/Assets/TT_Root/Scripts/Managers/WeaponManager.cs
@@ -3,36 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ChangeWeapon : MonoBehaviour
+public class WeaponManager : MonoBehaviour
 {
+    private static WeaponManager instance;
+    public static WeaponManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
 
+                Debug.Log("WeaponManager is null!");
+            }
+            return instance;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public enum Weapons { gun, bomber, bigarm, car }
 
     [SerializeField] GameObject gunG, bombG, armG, carG;
 
-    // Start is called before the first frame update
+
+    public Weapons actualWeapon;
+
     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 
     public void OnGun(InputAction.CallbackContext context)
     {
         gunG.SetActive(true); bombG.SetActive(false); armG.SetActive(false); carG.SetActive(false);
+        actualWeapon = Weapons.gun;
     }
     public void OnBomber(InputAction.CallbackContext context)
     {
         gunG.SetActive(false); bombG.SetActive(true); armG.SetActive(false); carG.SetActive(false);
+        actualWeapon = Weapons.bomber;
     }
 
     public void OnBigArm(InputAction.CallbackContext context)
     {
         gunG.SetActive(false); bombG.SetActive(false); armG.SetActive(true); carG.SetActive(false);
+        actualWeapon = Weapons.bigarm;
     }
 
     public void OnCar(InputAction.CallbackContext context)
     {
         gunG.SetActive(false); bombG.SetActive(false); armG.SetActive(false); carG.SetActive(true);
+        actualWeapon = Weapons.car;
     }
 }
--- Transmission Takedown/Assets/TT_Root/Scripts/PlayerScripts/GunSystem.cs
+++ Transmission Takedown/Assets/TT_Root/Scripts/PlayerScripts/GunSystem.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GunSystem : MonoBehaviour
 {
+    [Header("Attack Stats")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject shootPoint;
+    [SerializeField] bool canShoot;
+    [SerializeField] bool shooting;
+    public float lowCD;
+    public float fastSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,41 @@ public class GunSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (shooting) { Attacks(); }
     }
+
+    void Attacks()
+    {
+        switch(WeaponManager.Instance.actualWeapon) 
+        {
+            case WeaponManager.Weapons.gun:
+                if (canShoot)
+                {
+                    canShoot = false;
+                    Rigidbody rb = Instantiate(bullet, shootPoint.transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+                    rb.AddForce(transform.forward * fastSpeed, ForceMode.Impulse);
+                    Invoke(nameof(ResetShoot), lowCD);
+                }
+                break;
+        }
+    }
+
+    void ResetShoot()
+    {
+        canShoot = true;
+    }
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Dispara");
+            shooting = true;
+
+        }
+        if (context.canceled)
+        {
+            shooting = false;
+        }
+    }
+
 }
