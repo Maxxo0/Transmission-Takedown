f7c7ec9dbb8c218b08af08c3774069cce9ac0bc1
Maxxo0 <135011110+Maxxo0@users.noreply.github.com>
2024-05-08 13:14:06 +0200

    Bullet Arreglao y Toon con Outline creado

--- Transmission Takedown/Assets/TT_Root/Scripts/AttackScripts/Bullet.cs
+++ Transmission Takedown/Assets/TT_Root/Scripts/AttackScripts/Bullet.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    
+    [SerializeField] int bulletDamage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +23,31 @@ public class Bullet : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            enemyHealth.EnemyTakeDamage(bulletDamage);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.EnemyTakeDamage(bulletDamage);
+            Destroy(gameObject);
+            Debug.Log("Pega");
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+            Debug.Log("Pega");
         }
     }
 }
