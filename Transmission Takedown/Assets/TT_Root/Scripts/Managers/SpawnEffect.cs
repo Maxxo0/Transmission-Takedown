a763062012a927f3d3d7dc3ce57dee24323cd572
Maxxo0 <maximoamorebonapasta12@gmail.com>
2024-05-17 03:44:00 +0200

    SpawnEffect bueno

---
+++ Transmission Takedown/Assets/TT_Root/Scripts/Managers/SpawnEffect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEffect : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        Invoke(nameof(Chao), 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Chao()
+    {
+        gameObject.SetActive(false);
+    }
+}
